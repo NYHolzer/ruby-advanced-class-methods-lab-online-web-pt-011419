28e20c521d33362eccb4239927855991eae3a6b8
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-08 02:22:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
     info = filename.split(" ")
     song = self.new       #created a new song object
     #set to name of this song = name of song in file
-    song.name = info[1].remove 
+    song.name = info[1].chomp(".mp3")
   end
   
 end
